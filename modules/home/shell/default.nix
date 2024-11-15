3ba409a68e015fa25317bb0f848561fe0fa95aba
lfu <lfu@anduril.com>
2024-11-14 18:31:08 -0800

    shell adds

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -1,6 +1,9 @@
 { pkgs, username, ... }: {
   programs.fish = {
     enable = true;
+    shellInit = ''
+      set fish_greeting ""
+    '';
     interactiveShellInit = ''
       set fish_cursor_default line
     '';
@@ -13,6 +16,11 @@
   xdg.configFile."starship.toml".source = ./starship.toml;
 
   programs.zellij = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
